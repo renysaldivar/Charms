679e064add70a4780919930a9cf3aa950211d003
renata-sg <renata.saldivar97@gmail.com>
2019-10-14 18:11:52 -0500

    Adds params and return value to the function class

--- Function.py
+++ Function.py
@@ -1,5 +1,7 @@
 # Class to store information about a function
 
 class Function:
-	def __init__(self, funcId, varType, varScope):
+	def __init__(self, funcId, funcParams, funcReturnType):
 		self.funcId = funcId # unique identifier
+		self.funcParams = funcParams # list of params
+		self.funcReturnType = funcReturnType # type of the value returned by the function
