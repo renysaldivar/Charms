66c0e6c1fe8b8c99aa56c635fb07ec712abd19ff
renata-sg <renata.saldivar97@gmail.com>
2019-10-14 18:53:43 -0500

    Removes funcId from function class

--- Function.py
+++ Function.py
@@ -1,7 +1,6 @@
 # Class to store information about a function
 
 class Function:
-	def __init__(self, funcId, funcParams, funcReturnType):
-		self.funcId = funcId # unique identifier
+	def __init__(self, funcParams, funcReturnType):
 		self.funcParams = funcParams # list of params
 		self.funcReturnType = funcReturnType # type of the value returned by the function
