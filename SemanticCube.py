bb2b347aa27ecf97298c37ee0d1fea2bdc03603c
renata-sg <renata.saldivar97@gmail.com>
2019-10-14 22:06:28 -0500

    Fix logical operators

--- SemanticCube.py
+++ SemanticCube.py
@@ -1,29 +1,29 @@
 def isLessOrGreaterThan(operator):
-    return operator == '>' || operator == '<'
+    return operator == '>' or operator == '<'
 
 def isEqualOrNotEqual(operator):
-    return operator == '==' || operator == '!='
+    return operator == '==' or operator == '!='
 
 def arithmeticOperators(operator, type1, type2):
-    if type1 == 'int' && type2 == 'int':
+    if type1 == 'int' and type2 == 'int':
         return 'int'
     raise TypeError(TYPE_ERROR.format(operator, type1, type2))
 
 def relationalOperators(operator, type1, type2):
-    if (isLessOrGreaterThan(operator) && type1 == 'int' && type2 == 'int') || (isEqualOrNotEqual(operator) && type1 == type2):
+    if (isLessOrGreaterThan(operator) and type1 == 'int' and type2 == 'int') or (isEqualOrNotEqual(operator) and type1 == type2):
         return 'bool'
     raise TypeError(TYPE_ERROR.format(operator, type1, type2))
 
 def logicalOperators(operator, type1, type2):
-    if type1 == 'bool' && type2 == 'bool':
+    if type1 == 'bool' and type2 == 'bool':
         return 'bool'
     raise TypeError(TYPE_ERROR.format(operator, type1, type2))
 
 def validOperation(operator, type1, type2):
-    if operator == '+' || operator == '-' || operator == '*' || operator == '/':
+    if operator == '+' or operator == '-' or operator == '*' or operator == '/':
         return arithmeticOperators(operator, type1, type2)
-    if operator == '<' || operator == '>' || operator == '==' || operator == '!=':
+    if operator == '<' or operator == '>' or operator == '==' or operator == '!=':
         return relationalOperators(operator, type1, type2)
-    if operator == '&&' || operator == '||':
+    if operator == '&&' or operator == '||':
         return logicalOperators(operator, type1, type2)
     Exception("{} does not exist".format(operator))
