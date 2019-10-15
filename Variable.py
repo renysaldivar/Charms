1016db5da259daf5de07d9689d162ffa2aa7f09c
lorbichara <lorbichara@gmail.com>
2019-10-14 21:46:23 -0500

    Added get and insert variable functions

--- Variable.py
+++ Variable.py
@@ -1,7 +1,6 @@
 # Class to store information about a variable
 
 class Variable:
-	def __init__(self, varId, varType, varScope):
-		self.varId = varId # unique identifier
+	def __init__(self, varType, varScope):
 		self.varType = varType # int, char, bool
 		self.varScope = varScope # local or global
--- VariableTable.py
+++ VariableTable.py
@@ -1,7 +1,21 @@
 from Variable import Variable
 
 class VariableTable:
-	def __init__(self, vars, varId):
-		self.vars = {
-			varId: Variable()
-		}
+	def __init__(self, vars, keywords):
+		self.vars = {}
+		self.keywords = ["int", "void", "bool", "char", "if", "else", "while", "print", "read", "return", "function", "id"]
+
+	def getVariable(varId):
+		if varId in self.vars.keys():
+			return self.vars[varId]
+		else:
+			Exception("{} does not exist in the directory".format(varId))
+
+	def insertVariable(varId, varType, varScope):
+		if varId in self.vars.keys():
+			Exception("{} already exists in the directory".format(varId))
+		elif varId in self.keywords:
+			Exception("{} is a reserved word".format(varId))
+		else:
+			v = Variable(varType, varScope)
+			self.vars[varId] = v
