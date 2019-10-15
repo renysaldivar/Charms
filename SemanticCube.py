ab177e57b3854a1234aef2c1a5c933c1971f3342
renata-sg <renata.saldivar97@gmail.com>
2019-10-14 22:00:07 -0500

    Adds missin single quotation marks to types

--- SemanticCube.py
+++ SemanticCube.py
@@ -5,18 +5,18 @@ def isEqualOrNotEqual(operator):
     return operator == '==' || operator == '!='
 
 def arithmeticOperators(operator, type1, type2):
-    if type1 == int && type2 == int:
-        return int
+    if type1 == 'int' && type2 == 'int':
+        return 'int'
     raise TypeError(TYPE_ERROR.format(operator, type1, type2))
 
 def relationalOperators(operator, type1, type2):
-    if (isLessOrGreaterThan(operator) && type1 == int && type2 == int) || (isEqualOrNotEqual(operator) && type1 == type2):
-        return bool
+    if (isLessOrGreaterThan(operator) && type1 == 'int' && type2 == 'int') || (isEqualOrNotEqual(operator) && type1 == type2):
+        return 'bool'
     raise TypeError(TYPE_ERROR.format(operator, type1, type2))
 
 def logicalOperators(operator, type1, type2):
-    if type1 == bool && type2 == bool:
-        return bool
+    if type1 == 'bool' && type2 == 'bool':
+        return 'bool'
     raise TypeError(TYPE_ERROR.format(operator, type1, type2))
 
 def validOperation(operator, type1, type2):
