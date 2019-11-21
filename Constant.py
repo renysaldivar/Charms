44a592d677ea4ffe3da6d62d59e3e1942bf2f879
renata-sg <renata.saldivar97@gmail.com>
2019-11-21 12:47:12 -0600

    Adds constant and constant table class

---
+++ Constant.py
@@ -0,0 +1,6 @@
+# Class to store information about a constant
+
+class Constant:
+    def __init__(self, constantType, constantAddress):
+        self.constantType = constantType # int
+        self.constantAddress = constantAddress # memory address
