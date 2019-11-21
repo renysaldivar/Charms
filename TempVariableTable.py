e14334e30da2312582db5c49234007bc8a5e7234
renata-sg <renata.saldivar97@gmail.com>
2019-11-21 16:41:49 -0600

    Adds tempVariable and tempVariableTable class

---
+++ TempVariableTable.py
@@ -0,0 +1,17 @@
+from TempVariable import TempVariable
+
+class TempVariableTable:
+    def __init__(self, tempVariables):
+        self.tempVariables = {}
+
+    def insertTempVariable(self, tempVariableId, tempVariableType, tempVariableAddress):
+        if tempVariableId in self.tempVariables:
+            Exception("{} already exists in the directory".format(varId))
+        else:
+            tv = TempVariable(tempVariableType, tempVariableAddress)
+            self.tempVariables[tempVariableId] = tv
+
+    def printTable(self):
+		for tempVariable in self.tempVariables:
+			print(tempVariable, self.tempVariables[tempVariable].tempVariableType,
+                                self.tempVariables[tempVariable].tempVariableAddress)
