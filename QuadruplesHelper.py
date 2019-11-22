7d44b5d224c40314067e24ff21560429bcd34ad8
lorbichara <lorbichara@gmail.com>
2019-11-21 20:22:24 -0600

    First version of convert quadruples function - currently converting constants and global variables

--- QuadruplesHelper.py
+++ QuadruplesHelper.py
@@ -1,3 +1,24 @@
 def printQuadruples(quadruples):
     for quad in quadruples:
         quad.printQuad()
+
+def convertQuadruples(quadruples, functionDirectory, constantTable, varTable):
+    for quad in quadruples:
+        if quad.operator != 'ERA' and quad.operator != 'GOSUB':
+            leftOperand = quad.leftOperand
+            rightOperand = quad.rightOperand
+            variables = varTable.vars
+            constants = constantTable.constants
+            functions = functionDirectory.dictionary
+
+            # Left operand
+            if leftOperand in variables:
+                quad.leftOperand = variables[leftOperand].varAddress
+            elif leftOperand in constants:
+                quad.leftOperand = constants[leftOperand].constantAddress
+
+            # Right operand
+            if rightOperand in variables:
+                quad.rightOperand = variables[rightOperand].varAddress
+            elif rightOperand in constants:
+                quad.rightOperand = constants[rightOperand].constantAddress
