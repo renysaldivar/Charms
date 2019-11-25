f08d542d25cbff5c0a50d1cf8279132dbce60a37
renata-sg <renata.saldivar97@gmail.com>
2019-11-25 11:25:52 -0600

    Adds templates to CharmsApp

--- CharmsProject/CharmsApp/views.py
+++ CharmsProject/CharmsApp/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.template import RequestContext
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Test")
+    template = loader.get_template('index.html')
+    context = {}
+    index = template.render(context, request)
+    return HttpResponse(index)
