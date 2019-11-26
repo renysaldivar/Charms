54b024848d1b9a22c03174fe010ff9a3e6fdfdb0
lorbichara <lorbichara@gmail.com>
2019-11-25 21:04:38 -0600

    Minor change to drop function so it copies the DOM element instead of moving it

--- CharmsProject/CharmsApp/static/canvas.js
+++ CharmsProject/CharmsApp/static/canvas.js
@@ -9,5 +9,7 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    var nodeCopy = document.getElementById(data).cloneNode(true);
+    nodeCopy.id = "newId"; /* We cannot use the same ID */
+    ev.target.appendChild(nodeCopy);
 }
